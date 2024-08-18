2f917ff4bb05b36e942ca53be6ffaa6730f31a19
Kemboi Cheruiyot <87800637+KolKemboi@users.noreply.github.com>
2024-08-18 22:46:50 +0300

    update

--- C_Code/ClassCode.c
+++ C_Code/ClassCode.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 
-
+int min(int, int, int);
 
 int main() {
 	int num = 10;
 	int numb = 100;
-
-	printf("%d\n", max_fun(num, numb));
+	 
 	{
 		int a = 10;
 		printf("%d\n", a);
 		printf("%d\n", num);
 	}
+	int maximum = min(10, 18, 50);
+	printf("The lesser is %d\n", maximum);
 	printf("Code Has Run\n");
 	return 0;
+}
+
+int min(int a, int b, int c) {
+	int less =  ((a) > (b) ? (a) : (b));
+	int lesser =  ((less) > (c) ? (less) : (c));
+	return lesser;
 }
